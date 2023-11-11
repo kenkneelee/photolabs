8e70b3618b3c7208233b532aeba0f13c1afe7484
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 15:48:59 -0500

    remove temp sample data, move photolistitems into photolist component and import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,14 @@
 import React from "react";
 
-import PhotoListItem from "./components/PhotoListItem";
+import PhotoList from "./components/PhotoList";
 import "./App.scss";
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-// Just creating some sample data with identical ids. will be updated with unique ids later
-const samplePhotosData = new Array(3).fill(sampleDataForPhotoListItem)
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = samplePhotosData.map((photo) => {
-    return <PhotoListItem data={photo} key={photo.id} />;
-  });
   return (
     <div className="App">
-      {photos}
+      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+      <PhotoList />
     </div>
   );
 };
