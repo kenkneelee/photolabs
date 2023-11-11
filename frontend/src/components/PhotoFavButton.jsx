03155eb61f514d483043605aa46deffa91f96ac2
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 12:47:30 -0500

    add onclick state management & pass state to favicon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,18 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton() {
+  const [favorited, setFavorited] = useState(false);
+  const handleClick = () => {
+    setFavorited(!favorited);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={favorited} />
       </div>
     </div>
   );
