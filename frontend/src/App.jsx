3a535251af163a84f4071c14ae78475552f2931c
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 12:45:20 -0500

    refactor to accept an array of photo list data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,11 +14,17 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
+// Just creating some sample data with identical ids. will be updated with unique ids later
+const samplePhotosData = new Array(3).fill(sampleDataForPhotoListItem)
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photos = samplePhotosData.map((photo) => {
+    return <PhotoListItem data={photo} key={photo.id} />;
+  });
   return (
     <div className="App">
-      <PhotoListItem data = {sampleDataForPhotoListItem} key={sampleDataForPhotoListItem.id}/>
+      {photos}
     </div>
   );
 };
