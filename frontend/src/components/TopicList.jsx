315cc81d02f8fb540511d8f053f8857fb34554a9
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 18:49:48 -0500

    receive and render topic data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,33 +3,11 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
-  const topics = sampleDataForTopicList.map((topic) => {
-    return <TopicListItem key = {topic.id} data = {topic}/>
-  })
-  return (
-    <div className="top-nav-bar__topic-list">
-      {topics}
-    </div>
-  );
+const TopicList = (props) => {
+  const topics = props.topics.map((topic) => {
+    return <TopicListItem key={topic.id} data={topic} />;
+  });
+  return <div className="top-nav-bar__topic-list">{topics}</div>;
 };
 
 export default TopicList;
