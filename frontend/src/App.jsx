7a0244e12eece73893b13c7192444a7794ef2696
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 17:23:52 -0500

    add topiclist to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import TopicList from "components/TopicList";
 import PhotoList from "./components/PhotoList";
 import "./App.scss";
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="App">
       {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+      <TopicList />
       <PhotoList />
     </div>
   );
