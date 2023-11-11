72faf404a7ab976e749b4e0c32554c3e47e9de8c
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 18:17:45 -0500

    collect navbar and photolist components into homeroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
-import PhotoList from "./components/PhotoList";
-import TopNavigationBar from "./components/TopNavigationBar";
 import "./App.scss";
+import HomeRoute from "./components/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
       {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
