466eabaabd46511fb51db19e00a6f95ea60779a2
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 20:48:58 -0500

    pass isFavPhotoExist into FavBadge for conditional rendering

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,12 +5,12 @@ import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
 
 const TopNavigation = (props) => {
-  const { topics } = props;
+  const { topics, isFavPhotoExist } = props;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} />
-      <FavBadge />
+      <FavBadge isFavPhotoExist={isFavPhotoExist}/>
     </div>
   );
 };
