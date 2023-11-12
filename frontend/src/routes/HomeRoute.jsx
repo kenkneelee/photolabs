7158b2c84a65b16106034a931113d3e2caf2e163
Kenny Lee <kenny.lee927@gmail.com>
2023-11-11 20:32:36 -0500

    create favorited photos array state, monitor with useEffect, pass down to photolist

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import TopNavigation from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
 import "styles/HomeRoute.scss";
 
-
 const HomeRoute = (props) => {
-  const {topics, photos} = props;
+  const { topics, photos } = props;
+  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
+
+  // log updated favorites list on state change
+  useEffect(() => {
+    console.log("Favorites: ", favoritedPhotos);
+  }, [favoritedPhotos]);
 
   return (
     <div className="home-route">
-      <TopNavigation topics = {topics}/>
-      <PhotoList photos = {photos}/>
+      <TopNavigation topics={topics} />
+      <PhotoList
+        photos={photos}
+        favoritedPhotos={favoritedPhotos}
+        setFavoritedPhotos={setFavoritedPhotos}
+      />
     </div>
   );
 };
