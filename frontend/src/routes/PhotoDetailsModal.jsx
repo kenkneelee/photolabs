3932bce1c2cbb09cb3d1a3f29e50614a80819a90
Kenny Lee <kenny.lee927@gmail.com>
2023-11-13 11:00:08 -0500

    implement similar photos list & favorite button, fix favorites button across components

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -8,9 +8,36 @@ import PhotoFavButton from "components/PhotoFavButton";
 import PhotoList from "components/PhotoList";
 
 const PhotoDetailsModal = (props) => {
-  const { setModalVisibility, activePhoto } = props;
+  const {
+    photos,
+    favoritedPhotos,
+    setFavoritedPhotos,
+    setModalVisibility,
+    activePhoto,
+    setActivePhoto,
+  } = props;
   const handleCloseModal = () => {
     setModalVisibility(false);
+  };
+
+  // Convert the similar_photos object to an array of similar photos
+  const similarPhotosArray = Object.values(activePhoto.similar_photos);
+
+  // Extract the IDs of similar photos
+  const similarPhotoIds = similarPhotosArray.map((photo) => photo.id);
+
+  // Filter the photos array to include only the similar photos
+  const similarPhotos = photos.filter((photo) =>
+    similarPhotoIds.includes(photo.id)
+  );
+
+  // should refactor
+  const addToFavorites = () => {
+    !favoritedPhotos.some((photo) => photo.id === activePhoto.id)
+      ? setFavoritedPhotos([...favoritedPhotos, activePhoto])
+      : setFavoritedPhotos(
+          favoritedPhotos.filter((photo) => photo.id !== activePhoto.id)
+        );
   };
 
   return (
@@ -19,15 +46,19 @@ const PhotoDetailsModal = (props) => {
       <button className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" onClick={handleCloseModal} />
       </button>
-
+      {/* the photo */}
       <div className="photo-details-modal__images">
-        <PhotoFavButton />
+        <PhotoFavButton
+          addToFavorites={addToFavorites}
+          photo={activePhoto}
+          favoritedPhotos={favoritedPhotos}
+        />
         <img
           src={activePhoto.urls.full}
           alt="Image"
           className="photo-details-modal__image"
         />
-
+        {/* photographer details */}
         <div className="photo-details-modal__photographer-details">
           <img
             src={activePhoto.user.profile}
@@ -49,7 +80,15 @@ const PhotoDetailsModal = (props) => {
       <div className="photo-details-modal__images">
         <h2 className="photo-details-modal__header">Similar Images</h2>
       </div>
-      <PhotoList photos={Object.values(activePhoto.similar_photos)} />
+      {activePhoto && activePhoto.similar_photos && (
+        <PhotoList
+          photos={similarPhotos}
+          favoritedPhotos={favoritedPhotos}
+          setFavoritedPhotos={setFavoritedPhotos}
+          setModalVisibility={setModalVisibility}
+          setActivePhoto={setActivePhoto}
+        />
+      )}
     </div>
   );
 };
