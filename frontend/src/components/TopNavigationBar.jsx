bd80eebe994c7748b2180017cc03b90995cf1be8
Kenny Lee <kenny.lee927@gmail.com>
2023-11-13 16:56:51 -0500

    restore navbar to center

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -10,10 +10,8 @@ const TopNavigation = (props) => {
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       {/* moved TopicList right as per design feedback */}
-      <div className="top-nav-bar__right">
       <TopicList topics={topics} />
       <FavBadge isFavPhotoExist={isFavPhotoExist}/>
-      </div>
     </div>
   );
 };
