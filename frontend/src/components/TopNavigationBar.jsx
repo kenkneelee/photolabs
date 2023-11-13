d07c11fe66b3395204db29b17012fdfcf58d8a22
Kenny Lee <kenny.lee927@gmail.com>
2023-11-13 11:47:19 -0500

    move nav bar right as per design

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -9,8 +9,11 @@ const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      {/* moved TopicList right as per design feedback */}
+      <div className="top-nav-bar__right">
       <TopicList topics={topics} />
       <FavBadge isFavPhotoExist={isFavPhotoExist}/>
+      </div>
     </div>
   );
 };
