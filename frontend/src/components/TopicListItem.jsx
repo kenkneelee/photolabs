c1d58076f2343f5c0488dc76c13381a6454c5071
Kenny Lee <kenny.lee927@gmail.com>
2023-11-15 23:47:07 -0500

    add click handler to use onLoadTopic

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,17 +2,15 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
-
 const TopicListItem = (props) => {
-  const { data } = props;
+  const { data, onLoadTopic } = props;
+  const handleTopicClick = () => {
+    onLoadTopic(data);
+  }
+
   return (
     <div className="topic-list__item">
-      <span>{data.title}</span>
+      <span onClick={handleTopicClick}>{data.title}</span>
     </div>
   );
 };
